abf128e0c6d502ee702bed16c3332005bacd30aa
Bryan <bryan.londono@ingeneo.com.co>
2019-10-31 09:02:13 -0500

    Prueba

--- Xm.Acme.Pagos/Applicacion.Expirations.WebApi/Program.cs
+++ Xm.Acme.Pagos/Applicacion.Expirations.WebApi/Program.cs
@@ -17,6 +17,7 @@ namespace Applicacion.IntegrationOracle.WebApi
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             // Inicialize Automapper
